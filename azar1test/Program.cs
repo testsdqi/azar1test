66c1be6b7de3258ac5bcd4cccd31d521899e96cf
esmaeil1378 <esmaeilrezaei13781222@gmail.com>
2023-11-22 11:09:24 +0330

    edited byb essse

--- azar1test/Program.cs
+++ azar1test/Program.cs
@@ -1,3 +1,14 @@
 ﻿Console.Write("enter name:");
 string str = Console.ReadLine();
 Console.WriteLine("name:  "+str);
+
+
+
+
+Console.WriteLine("plaese enter a name :");
+string name = Console.ReadLine();
+for (int i = 0; i < name.Length; i++)
+{
+    Console.WriteLine($"name is :{name}");
+    Console.WriteLine($"number is :{i}");
+}
