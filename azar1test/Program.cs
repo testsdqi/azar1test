22b535fb6e61e9141bb562b215bc3c094cfa4a94
testsdqi <perestashop2050@gmail.com>
2023-11-22 10:26:50 +0330

    test1

--- azar1test/Program.cs
+++ azar1test/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("enter name:");
+string str = Console.ReadLine();
+Console.WriteLine(str);
